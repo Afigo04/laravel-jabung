248d025b4e96a82529c0495326e3e58eaea54405
LurahKosKembar <titanapril29@gmail.com>
2024-08-01 14:35:16 +0700

    add footer and header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,19 +13,41 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect" />
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
 
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
     <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
 
     <!-- Main CSS File -->
-    <link href="assets/css/main.css" rel="stylesheet" />
-    <link href="assets/css/navbar.css" rel="stylesheet" />
-    <link href="assets/css/footer.css" rel="stylesheet" />
-    <link href="assets/css/anggaran.css" rel="stylesheet">
+    <link href="{{asset('css/main.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/navbar.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/footer.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/' . $style . '.css') }}" rel="stylesheet">
 </head>
-<body></body>
+
+<body>
+    <header id="header" class="header d-flex align-items-center fixed-top">
+        <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+            <div class="custom-navbar">
+                <img src="{{asset('img/logo-ponorogo')}}" alt="Logo Ponorogo" />
+                <div class="nama-logo">
+                    <h1 class="sitename">Desa Jabung</h1>
+                    <span style="font-size: 14px">Kec. Mlarak. Kab. Ponorogo</span> <br />
+                    <span style="font-size: 14px">Jawa Timur</span>
+                </div>
+            </div>
+
+            <nav id="navmenu" class="navmenu">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">Profil Desa</a></li>
+                    <li><a href="surat-menyurat.html">Surat Menyurat</a></li>
+                    <li><a href="anggaran.html">Anggaran</a></li>
+                    <li><a href="blog.html">Galery</a></li>
+                    <li><a href="contact.html">Pengaduan</a></li>
+                </ul>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+        </div>
+    </header>
