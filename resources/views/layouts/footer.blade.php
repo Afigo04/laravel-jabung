d27909dac06e7300d88fb1ca4cfd2cda7adc612f
jalusena <kayumanis101@gmail.com>
2024-09-07 00:16:17 +0700

    background and footer fixes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,25 +3,26 @@
         <div class="d-flex flex-column justify-content-center align-items-center gap-2">
             <div class="d-flex gap-3 custom-icon">
                 <a target="_blank" href="dsjabung@gmail.com">
-                    <img src="{{asset('img/email-logo.png')}}" alt="email" />
+                    <img src="{{asset('img/em-icon.png')}}" alt="email" />
                 </a>
                 <a target="_blank" href="https://www.instagram.com/pemdes_jabung">
-                    <img src="{{asset('img/ig-logo.png')}}" alt="logo" />
+                    <img src="{{asset('img/ig-icon.png')}}" alt="logo" />
                 </a>
-                <a target="_blank" href="#">
-                    <img src="{{asset('img/fb-logo.png')}}" alt="facebook" />
+                <a target="_blank" href="https://www.tiktok.com/@pandhito.wesi.kun?_t=8pSUZypuvxp&_r=1">
+                    <img src="{{asset('img/tik-icon.png')}}" alt="facebook" />
                 </a>
                 <a target="_blank" href="https://www.youtube.com/@pandhitowesikuning1922?si=B7WDsqRLVstmH6Nm">
-                    <img src="{{asset('img/ytb-logo.png')}}" alt="youtube" />
+                    <img src="{{asset('img/yt-icon.png')}}" alt="youtube" />
                 </a>
             </div>
         </div>
         <hr style="border-bottom: 3px solid #fff" />
         <div class="d-flex flex-column gap-1 justify-content-between">
             <div class="text-white footer-text">
-                <p class="text-center mb-2 text-white">Jl. Nama Jalan no jalan, Ponorogo, Jawa Timur, Indonesia</p>
+                <p class="text-center mb-2 text-white">Jl. Arjuna No. 31, Desa Jabung, Kec. Mlarak, Ponorogo</p>
             </div>
-            <div class="text-white text-center">© 2024 Dinas Komunikasi, Informasi dan Statistik Desa Jabung</div>
+            <div class="text-white text-center">
+                <a href="https://www.instagram.com/pengoding_tulus?igsh=MWxmZzFkM215Ynk1MQ==">© 2024 Sincere Burch Project </a></div>
         </div>
     </div>
 </footer>
